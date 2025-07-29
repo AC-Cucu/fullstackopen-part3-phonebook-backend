67cd447901628887c7bc075fd2e7fc04603d9866
alexandru.cucuruzeanu <alexandrucristian.cucuruzeanu@gmail.com>
2025-07-29 10:00:07 +0200

    Part3 - Node.js & Express - Phonebook - Step 11 - Full Stack;

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 
 app.use(express.json())
-// app.use(express.static('dist'))
+app.use(express.static('dist'))
 app.use(cors())
 
 morgan.token('post-body', (req) => {
