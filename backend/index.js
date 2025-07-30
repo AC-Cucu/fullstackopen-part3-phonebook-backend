0dff7a8db342c66a392603eb9b1ded274938f762
Alexandru Cristian Cucuruzeanu <alexandrucristian.cucuruzeanu@gmail.com>
2025-07-30 10:20:28 +0200

    Part3 - Node.js & Express - Phonebook - Database - Step 7  - adding validation to the schema and errorHandler;

--- backend/index.js
+++ backend/index.js
@@ -68,7 +68,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
     //const personExists = phonebook.find(person => person.name === body.name)
@@ -95,23 +95,23 @@ app.post('/api/persons', (request, response) => {
         number: body.number
     })
 
-    person.save().then(savedPerson => {
+    person.save()
+    .then(savedPerson => {
         console.log('Person created successfully');
         response.json(savedPerson)
     })
+    .catch(error => next(error))
 })
 
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const body = request.body
+    const {name, number} = request.body
 
-    const person = {
-        name: body.name,
-        number: body.number
-    }
-
-    Phonebook
-    .findByIdAndUpdate(request.params.id, person, { new: true })
+    Phonebook.findByIdAndUpdate(
+        request.params.id
+        , {name, number}
+        , { new: true, runValidators: true, context: 'query' }
+    )
     .then(updatedPerson => response.json(updatedPerson))
     .catch(error => next(error))
 })
@@ -127,6 +127,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error?.name === 'CastError' || error?.name === 'TypeError') {
         return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: 'Name should be at least 3 characters!' })
     }
 
     next(error)
